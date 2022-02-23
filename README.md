ff1d42ce1035e0debbbbd8736de91b2f45972be0
rajua <rajua@qti.qualcomm.com>
2022-02-23 20:15:22 +0530

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # remove-ide-files
-Removes the IDE files
+Removes the following IDE files
+*   .classpath
+*   .project
+*   .factorypath
+*   .settings
+
+Excludes the files whose path contains <i>node_modules</i>
+
+# Tech Stack 
+*   JDK 11
+*   Maven 3
+
+# Usage
+<code>mvn clean install</code>
+
+<code>java -jar remove-ide-files.jar [location [maxDepth]]<code>
--- src/main/java/com/felix/remove/ide/files/RemoveIdeFiles.java
+++ src/main/java/com/felix/remove/ide/files/RemoveIdeFiles.java
@@ -17,14 +17,19 @@ public class RemoveIdeFiles {
 	private static final boolean DEBUG = false;
 	private static final List<String> IDE_FILENAMES = Arrays.asList(".classpath", ".project", ".factorypath",
 			".settings");
+	private static final String EXCLUDE_PATH_NAME = "node_modules";
 
 	public static void main(String[] args) throws IOException {
 		String location = "C:\\Users\\rajua\\delete";
+		int depth = 2;
 		if (args != null && args.length > 0) {
 			location = args[0];
+			if (args.length > 1) {
+				depth = Integer.parseInt(args[1]);
+			}
 		}
 
-		System.out.println("Given location: " + location);
+		System.out.println("Given location: " + location + "; depth: " + depth);
 
 		Path locationPath = Paths.get(location);
 		if (Files.notExists(locationPath)) {
@@ -37,24 +42,27 @@ public class RemoveIdeFiles {
 		}
 
 		System.out.println("Working on location: " + locationPath.toAbsolutePath());
-		try (Stream<Path> walkStream = Files.walk(locationPath, 2)) {
-			walkStream.filter(sub -> IDE_FILENAMES.contains(sub.getFileName().toString())).forEach(deletingPath -> {
-				System.out.println("Deleting: " + deletingPath);
-				try (Stream<Path> deletableStream = Files.walk(deletingPath)) {
-					deletableStream.sorted(Comparator.reverseOrder()).forEach(delPath -> {
-						try {
-							Files.delete(delPath);
-							if (DEBUG) {
-								System.out.println(" -- Deleted --> " + delPath);
-							}
+		System.out.println("Excluding the files contains path name: " + EXCLUDE_PATH_NAME);
+		try (Stream<Path> walkStream = Files.walk(locationPath, depth)) {
+			walkStream.filter(sub -> !sub.toString().contains(EXCLUDE_PATH_NAME))
+					.filter(sub -> IDE_FILENAMES.contains(sub.getFileName().toString())).forEach(deletingPath -> {
+						System.out.println("Deleting: " + deletingPath);
+						try (Stream<Path> deletableStream = Files.walk(deletingPath)) {
+							deletableStream.sorted(Comparator.reverseOrder()).forEach(delPath -> {
+								try {
+									Files.delete(delPath);
+									if (DEBUG) {
+										System.out.println(" -- Deleted --> " + delPath);
+									}
+								} catch (Exception e) {
+									System.err.println(
+											"Errror while deleting the file: " + delPath + "; " + e.getMessage());
+								}
+							});
 						} catch (IOException e) {
-							System.err.println("Errror while deleting the file: " + delPath + "; " + e.getMessage());
+							throw new RuntimeException(e);
 						}
 					});
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			});
 		}
 	}
 }
